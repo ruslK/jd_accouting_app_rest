a935ce95337974e49a8c0f6ec64f9268623ce069
Ruslan Kasymov <russkasymov@gmail.com>
2021-03-15 22:06:43 -0400

    Set up project

---
+++ src/test/java/com/accountapp/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountapp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
